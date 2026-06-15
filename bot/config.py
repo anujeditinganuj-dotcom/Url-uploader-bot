eea0582e7e54bd3d1b6dad967e2eb8a33f2e74fd
anujeditinganuj-dotcom <anujeditinganuj@gmail.com>
2026-06-16 03:08:08 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,15 +5,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Bot Configuration
-API_ID = os.getenv("API_ID", "34446649")
-API_HASH = os.getenv("API_HASH", "8dc570c08d8e35e88fb9bfc73c65d7fa")
+API_ID = os.getenv("API_ID", "37476811")
+API_HASH = os.getenv("API_HASH", "7aa60670b871050820086c6267371ee6")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8620719393:AAEuJiGaMukiAYbOVN1zXAxDuz2sznkvYx0")
 MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
 DB_NAME = os.getenv("DB_NAME", "ytdl_bot_db")
-ADMINS = list(map(int, os.getenv("ADMINS", "7892805795").split()))
+ADMINS = list(map(int, os.getenv("ADMINS", "8730393744").split()))
 
 # List of additional IDs to add
-ADDITIONAL_IDS = [7892805795]  # Add as many as needed
+ADDITIONAL_IDS = [8730393744]  # Add as many as needed
 
 # Extend the ADMINS list with additional IDs
 ADMINS.extend(ADDITIONAL_IDS)
@@ -21,7 +21,7 @@ ADMINS.extend(ADDITIONAL_IDS)
 # Remove duplicates (if any) and ensure it's a list
 ADMINS = list(set(ADMINS))
 #ADMINS = list(map(int, os.getenv("ADMINS", "7361945688, 1596559467").split()))
-PAID_USERS = list(map(int, os.getenv("PAID_USERS", "7892805795").split()))
+PAID_USERS = list(map(int, os.getenv("PAID_USERS", "8730393744").split()))
 
 # Default Settings
 DEFAULT_UPLOAD_MODE = "video"  # 'video' or 'file'
